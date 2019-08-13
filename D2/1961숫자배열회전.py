a99b555e81ab6e072af3ebf227a3a5948ccc9ddc
조성원 <sungwoncho94@naver.com>
2019-08-13 17:54:38 +0900

    알고리즘 day03 문제풀이
     - D2
     - intermediate

--- D2/1961숫자배열회전.py
+++ D2/1961숫자배열회전.py
@@ -1,60 +1,50 @@
-'''
-2
-3
-1 2 3
-4 5 6
-7 8 9
-6
-6 9 4 7 0 5
-8 9 9 2 6 5
-6 8 5 4 9 8
-2 2 7 7 8 4
-7 5 1 9 7 9
-8 9 3 9 7 6
-#1
-741 987 369
-852 654 258
-963 321 147
-#2
-872686 679398 558496
-952899 979157 069877
-317594 487722 724799
-997427 894586 495713
-778960 562998 998259
-694855 507496 686278
-'''
 import copy
 
 T = int(input())
 
 for t in range(1, T+1):
     N = int(input())
-
     matrix = []
-    re_matrix = []
-    temp = []
-    result_matrix = [[0] * 3 for n in range(N)]
-    print(result_matrix)
+    re_list = []  # 90도 회전하면 나오는 숫자를 순서대로 넣은 리스트
+    re_matrix = [[0] * N for n in range(N)]  # re_list를 N * N 매트리스로 만들기
+    result_matrix = [[0] * N for n in range(N)]  # 
     
+    #input 매트리스 생성
     for n in range(N):
-        n_list = list(map(int, input().split()))
-        # list 1 2 3 -> relist 3 2 1 로 바꾼 후 rematrix에 append 하자
-        matrix.insert(0, n_list)
-
-        # nlist = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] / [[list1] [list2] [list3]]
-        # matrix = [[7, 8, 9], [4, 5, 6], [1, 2, 3]]
-        # re_matrix = 3 [1, 2, 3], 2 [4, 5, 6], 1 [7, 8, 9]
+        numlist = list(map(int, input().split()))
+        matrix.append(numlist)
     
-    cnt = 0
-    while cnt < N:
-        re_matrix = list(zip(matrix[0], matrix[1]=, matrix[2]))
-        for n in range(N):
-            result_matrix[n][cnt] = re_matrix[n]
-            temp.insert(0, re_matrix[n])
-        re_matrix = temp
+    row = 0
+    
+    # result_matrix 완성
+    while row < N:
+        # 첫 열의 역순으로 훑어가는 숫자리스트를 만들어서, re_matrix에 넣자.
+        for i in range(0, N):  # 열은 순서대로 훑고,
+            for j in range(N-1, -1, -1):  # 행은 역순으로 훑자.
+                re_list.append(matrix[j][i])
         
-        cnt += 1
+        # 90도 회전된 re_matrix완성
+        for i in range(N):
+            for j in range(N):
+                re_matrix[i][j] = re_list[0]
+                re_list.remove(re_list[0])
+
+        for i in range(N):
+            result_matrix[i][row] = re_matrix[i]
+        
+        # matrix = re_matrix
+        # re_matrix와 re_list 초기화
+        matrix = copy.deepcopy(re_matrix)
+        re_matrix = [[0] * N for n in range(N)]
+        re_list = []
+        row += 1
     
-    print(result_matrix)
-
-
+    print('#%d' % t)
+    for i in range(N):
+        for j in range(3):
+            for n in range(N):
+                if n == N-1:
+                    print(result_matrix[i][j][n], end=' ')
+                else:
+                    print(result_matrix[i][j][n], end='')
+        print()
