340563e1ae0839a90ea6ce7e380b10fd6d64046c
조성원 <sungwoncho94@naver.com>
2020-01-14 08:17:34 +0900

    5653줄기세포_ing : 예시2번 hour=4와 같이 활성화 된 세포를 죽은 세포로 측정하는 것이 문제

--- advanced/5653줄기세포_ing.py
+++ advanced/5653줄기세포_ing.py
@@ -21,11 +21,6 @@ def extend_right(mat):
     for n in range(max_n):
         mat[n].append(0)
 
-def in_matrix(x, y):
-    if 0 <= x < max_n and 0 <= y < max_m:
-        return True
-    return False
-
 T = int(input())
 
 for t in range(1, T+1):
@@ -46,10 +41,12 @@ for t in range(1, T+1):
     dir_i = [1, 0, -1, 0]
     dir_j = [0, 1, 0, -1]
 
+    # matrix 만들기
     for n in range(N):
         temp_list = list(map(int, input().split()))
         matrix.append(temp_list)
 
+    # matrix에 있는 세포를 state에 옮겨주기 (세포크기 + 1 = 활성화 시간)
     for i in range(N):  # 세로
         for j in range(M):  # 가로
             if matrix[i][j] > 0:
@@ -57,59 +54,55 @@ for t in range(1, T+1):
                 result2 += 1
 
     while hour < K:
-        print('==========', 'hour', hour, '==========')
+        # print('==========', 'hour', hour, '==========')
         # 확장해야하는 칸이 있는지부터 확인하자
-        for j in range(max_m):
-            if state[0][j] == 1:
+        # print('state')
+        # matprint(state)
+        for a in range(max_m):
+            if state[0][a] == 1:
                 extend_up(matrix)
                 extend_up(state)
                 max_n += 1
                 break
 
-        for j in range(max_m):
-            if state[max_n -1][j] == 1:
+        for b in range(max_m):
+            if state[max_n -1][b] == 1:
                 extend_down(matrix)
                 extend_down(state)
                 max_n += 1
                 break
 
-        for i in range(max_n):
-            if state[i][0] == 1:
+        for c in range(max_n):
+            if state[c][0] == 1:
                 extend_left(matrix)
                 extend_left(state)
                 max_m += 1
                 break
-        
-        for i in range(max_n):
-            if state[i][max_m - 1] == 1:
+
+        for d in range(max_n):
+            if state[d][max_m - 1] == 1:
                 extend_right(matrix)
                 extend_right(state)
                 max_m += 1
                 break
-        
-        # print('------바뀌기 전/줄추가 matrix, state ---------')
-        # matprint(matrix)
-        # print('---------')
-        # matprint(state)
-        # state에서 1인 값을 찾아서 번식
-        # 이 때, matrix값이 큰 것부터 찾아서 번식시켜야 한다
-        # matrix에서 번식이 완료된 칸은 'X'로 표시하기
-        # state == 1이었던 칸은 0으로 만든다
-        # 모든 번식이 끝난 후, state에서 0보다 큰 값들을 -1 한다
-        # matrix -> state로 다시 옮겨오기
-        for i in range(max_n):
-            for j in range(max_m):
+
+        for i in range(max_n):  # 세로
+            for j in range(max_m):  # 가로
+                # state == 1일때만 (번식을 해야 할 때만) 코드를 진행
                 if state[i][j] != 1 :
                     continue
+                # state == 1인 좌표의 하, 우, 상, 좌 방향으로 살펴본다
                 for dir in range(4):
                     dx = dir_i[dir] + i
                     dy = dir_j[dir] + j
-                    if state[dx][dy] == 0 : #matrix[dx][dy] == 0:
+                    # 번식하는 세포 주변이 빈공간이라면, 번식세포의 크기만큼 퍼뜨린다.
+                    # 이 때, matrix는 세포로 채워지지만 state의 주변은 채워지지 않는다
+                    # -> state가 0인 부분을 기준으로 삼아서, 원래 있던 세포와 새로 번식하려는 세포의 크기를 비교하여 번식시킨다
+                    if state[dx][dy] == 0:
                         matrix[dx][dy] = max(matrix[i][j], matrix[dx][dy])
-
+                # 네방향 번식이 끝나면, 번식이 끝난 세포는 죽은 세포로 바꾼다
                 matrix[i][j] = -1
                 state[i][j] = -1
-                result2 -= 1
 
 
         for i in range(max_n):
@@ -122,7 +115,7 @@ for t in range(1, T+1):
                     state[i][j] = matrix[i][j] + 1
         
         hour += 1
-        print("-------바뀐 후 matrix, state-------")
+        print("-------hour-------", hour)
         matprint(matrix)
         print('state')
         matprint(state)
@@ -134,3 +127,7 @@ for t in range(1, T+1):
 
     print('#{} {}'.format(t, result))
     #print('#{} {}'.format(t, result2))
+
+
+# 문제점 : 예시2번 hour = 4 의 모양이 틀리다.
+# 활성화되어 번식 된 세포를 바로 죽여버린 것이 문제 -> 수정
